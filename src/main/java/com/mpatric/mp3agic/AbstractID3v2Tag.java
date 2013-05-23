7d06d0a8217f4770fb1a2939838fc0e62ac896a1
Christoph Beck <beck@odysseus.de>
2013-05-23 14:28:46 +0200

    fixed: "." used for regular expression

--- src/main/java/com/mpatric/mp3agic/AbstractID3v2Tag.java
+++ src/main/java/com/mpatric/mp3agic/AbstractID3v2Tag.java
@@ -250,13 +250,13 @@ public abstract class AbstractID3v2Tag implements ID3v2 {
 			BufferTools.stringIntoByteBuffer(FOOTER_TAG, 0, FOOTER_TAG.length(), bytes, offset);
 		} catch (UnsupportedEncodingException e) {
 		}
-		String s[] = version.split(".");
+		String s[] = version.split("\\.");
 		if (s.length > 0) {
 			byte majorVersion = Byte.parseByte(s[0]);
 			bytes[offset + MAJOR_VERSION_OFFSET] = majorVersion;
 		}
 		if (s.length > 1) {
-			byte minorVersion = Byte.parseByte(s[0]);
+			byte minorVersion = Byte.parseByte(s[1]);
 			bytes[offset + MINOR_VERSION_OFFSET] = minorVersion;
 		}
 		packFlags(bytes, offset);
