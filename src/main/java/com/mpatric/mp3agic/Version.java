e9ea8d3abd372864277262af41c9afdfd9e59deb
Michael Patricios <michael.patricios@gmail.com>
2013-06-01 18:45:44 +0100

    Bump version string to 0.8.2 to match snapshot

--- src/main/java/com/mpatric/mp3agic/Version.java
+++ src/main/java/com/mpatric/mp3agic/Version.java
@@ -1,7 +1,7 @@
 package com.mpatric.mp3agic;
 
 public class Version {
-	private static final String VERSION = "0.8.1";
+	private static final String VERSION = "0.8.2";
 	private static final String URL = "http://github.com/mpatric/mp3agic";
 	
 	public static String asString() {
