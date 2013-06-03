339f4823c8f9dc4caf5aa28fe86812cef30616fd
Christoph Beck <beck@odysseus.de>
2013-06-03 16:34:31 +0200

    yet another test for ID3v2Tag.setGenreDescription(String)

--- src/test/java/com/mpatric/mp3agic/ID3v2TagTest.java
+++ src/test/java/com/mpatric/mp3agic/ID3v2TagTest.java
@@ -238,7 +238,23 @@ public class ID3v2TagTest extends TestCase {
 		String genre = BufferTools.byteBufferToString(bytes, 1, bytes.length - 1);
 		assertEquals("Jazz", genre);
 	}
-	
+
+	public void testSetGenreDescriptionOn24TagWithUnknownGenre() throws Exception {
+		ID3v2 id3tag = new ID3v24Tag();
+		setTagFields(id3tag);
+		id3tag.setGenreDescription("Bebop");
+		assertEquals("Bebop", id3tag.getGenreDescription());
+		assertEquals(-1, id3tag.getGenre());
+
+		Map<String, ID3v2FrameSet> frameSets = id3tag.getFrameSets();
+		ID3v2FrameSet frameSet = (ID3v2FrameSet) frameSets.get("TCON");
+		List<ID3v2Frame> frames = frameSet.getFrames();
+		ID3v2Frame frame = (ID3v2Frame) frames.get(0);
+		byte[] bytes = frame.getData();
+		String genre = BufferTools.byteBufferToString(bytes, 1, bytes.length - 1);
+		assertEquals("Bebop", genre);
+	}
+
 	public void testShouldReadCombinedGenreInTag() throws Exception {
 		ID3v2 id3tag = new ID3v23Tag();
 		setTagFields(id3tag);
