1e28b6f0ec94cf62193865493bd07dde1d2d0c34
Clint Burford <cburford@evernote.com>
2014-10-07 22:26:12 -0700

    Set iTunes comment correctly

--- src/main/java/com/mpatric/mp3agic/AbstractID3v2Tag.java
+++ src/main/java/com/mpatric/mp3agic/AbstractID3v2Tag.java
@@ -591,7 +591,7 @@ public abstract class AbstractID3v2Tag implements ID3v2 {
 	public void setItunesComment(String itunesComment) {
 		if (itunesComment != null && itunesComment.length() > 0) {
 			invalidateDataLength();
-			ID3v2CommentFrameData frameData = new ID3v2CommentFrameData(useFrameUnsynchronisation(), ITUNES_COMMENT_DESCRIPTION, null, new EncodedText(itunesComment));
+			ID3v2CommentFrameData frameData = new ID3v2CommentFrameData(useFrameUnsynchronisation(), "eng", new EncodedText(ITUNES_COMMENT_DESCRIPTION), new EncodedText(itunesComment));
 			addFrame(createFrame(ID_COMMENT, frameData.toBytes()), true);
 		}
 	}
