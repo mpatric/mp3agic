73c412f06d3c2d35ecab28267b558d5035dba5d1
TriangularRoot <->
2014-12-11 16:17:43 +0400

    Add File constructors to Mp3File
    Closes issue #51
    
    Adds constructors to Mp3File which takes File instead of String:
    
    public Mp3File(File file)
    public Mp3File(File file, int bufferLength)
    public Mp3File(File file, int bufferLength, boolean scanFile)
    
    Since filepath String is not supplied during construction, it is
    determined via file.getPath() method. In order to ensure consistent
    behavior independent of how Mp3File was constructed:
    
     - [filename] variable has been removed from Mp3File's superclass
       FileWrapper
     - all methods that were referring to it now refer to file.getPath()
    
    In order to prevent file separator related issues ("src/test/x.mp3" is a
    valid argument for Mp3File(String filename) constructor, but e.g. on
    Windows file.getPath() would return "src\test\x.mp3"):
    
     - Mp3File's save() method now compares File objects instead of String
       filepaths during equality test
     - String literals in form of "src/test/x.mp3" in test cases were replaced
       by "src" + fs + "test" + fs + "x.mp3" where [fs] stands for
       File.separator

--- src/test/java/com/mpatric/mp3agic/FileWrapperTest.java
+++ src/test/java/com/mpatric/mp3agic/FileWrapperTest.java
@@ -1,5 +1,6 @@
 package com.mpatric.mp3agic;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -8,14 +9,16 @@ import com.mpatric.mp3agic.FileWrapper;
 import junit.framework.TestCase;
 
 public class FileWrapperTest extends TestCase {
-
-	private static final String VALID_FILENAME = "src/test/resources/notags.mp3";
+	private static final String fs = File.separator;
+	private static final String VALID_FILENAME = "src" + fs + "test" + fs + "resources" + fs + "notags.mp3";
 	private static final long VALID_FILE_LENGTH = 2869;
 	private static final String NON_EXISTANT_FILENAME = "just-not.there";	
 	private static final String MALFORMED_FILENAME = "malformed.?";
 
 	public void testShouldReadValidFile() throws IOException {
 		FileWrapper fileWrapper = new FileWrapper(VALID_FILENAME);
+		System.out.println(fileWrapper.getFilename());
+		System.out.println(VALID_FILENAME);
 		assertEquals(fileWrapper.getFilename(), VALID_FILENAME);
 		assertTrue(fileWrapper.getLastModified() > 0);
 		assertEquals(fileWrapper.getLength(), VALID_FILE_LENGTH);
@@ -41,7 +44,16 @@ public class FileWrapperTest extends TestCase {
 	
 	public void testShouldFailForNullFilename() throws IOException {
 		try {
-			new FileWrapper(null);
+			new FileWrapper((String)null);
+			fail("NullPointerException expected but not thrown");
+		} catch (NullPointerException e) {
+			// expected
+		}
+	}
+	
+	public void testShouldFailForNullFilenameFile() throws IOException {
+		try {
+			new FileWrapper((java.io.File)null);
 			fail("NullPointerException expected but not thrown");
 		} catch (NullPointerException e) {
 			// expected
