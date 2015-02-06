ac6f7cfdeb7dffff5e453f886d0e229484b38cbf
Jan-Hendrik Peters <hennr@hennr.name>
2015-02-06 20:40:22 +0100

    make more use of junit 4

--- src/test/java/com/mpatric/mp3agic/FileWrapperTest.java
+++ src/test/java/com/mpatric/mp3agic/FileWrapperTest.java
@@ -1,62 +1,47 @@
 package com.mpatric.mp3agic;
 
+import org.junit.Test;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import com.mpatric.mp3agic.FileWrapper;
+import static org.junit.Assert.*;
 
-import junit.framework.TestCase;
+public class FileWrapperTest {
+    private static final String fs = File.separator;
+    private static final String VALID_FILENAME = "src" + fs + "test" + fs + "resources" + fs + "notags.mp3";
+    private static final long VALID_FILE_LENGTH = 2869;
+    private static final String NON_EXISTANT_FILENAME = "just-not.there";
+    private static final String MALFORMED_FILENAME = "malformed.?";
 
-public class FileWrapperTest extends TestCase {
-	private static final String fs = File.separator;
-	private static final String VALID_FILENAME = "src" + fs + "test" + fs + "resources" + fs + "notags.mp3";
-	private static final long VALID_FILE_LENGTH = 2869;
-	private static final String NON_EXISTANT_FILENAME = "just-not.there";	
-	private static final String MALFORMED_FILENAME = "malformed.?";
+    @Test
+    public void shouldReadValidFile() throws IOException {
+        FileWrapper fileWrapper = new FileWrapper(VALID_FILENAME);
+        System.out.println(fileWrapper.getFilename());
+        System.out.println(VALID_FILENAME);
+        assertEquals(fileWrapper.getFilename(), VALID_FILENAME);
+        assertTrue(fileWrapper.getLastModified() > 0);
+        assertEquals(fileWrapper.getLength(), VALID_FILE_LENGTH);
+    }
 
-	public void testShouldReadValidFile() throws IOException {
-		FileWrapper fileWrapper = new FileWrapper(VALID_FILENAME);
-		System.out.println(fileWrapper.getFilename());
-		System.out.println(VALID_FILENAME);
-		assertEquals(fileWrapper.getFilename(), VALID_FILENAME);
-		assertTrue(fileWrapper.getLastModified() > 0);
-		assertEquals(fileWrapper.getLength(), VALID_FILE_LENGTH);
-	}
-	
-	public void testShouldFailForNonExistentFile() throws IOException {
-		try {
-			new FileWrapper(NON_EXISTANT_FILENAME);
-			fail("FileNotFoundException expected but not thrown");
-		} catch (FileNotFoundException e) {
-			// expected
-		}
-	}
-	
-	public void testShouldFailForMalformedFilename() throws IOException {
-		try {
-			new FileWrapper(MALFORMED_FILENAME);
-			fail("FileNotFoundException expected but not thrown");
-		} catch (FileNotFoundException e) {
-			// expected
-		}
-	}
-	
-	public void testShouldFailForNullFilename() throws IOException {
-		try {
-			new FileWrapper((String)null);
-			fail("NullPointerException expected but not thrown");
-		} catch (NullPointerException e) {
-			// expected
-		}
-	}
-	
-	public void testShouldFailForNullFilenameFile() throws IOException {
-		try {
-			new FileWrapper((java.io.File)null);
-			fail("NullPointerException expected but not thrown");
-		} catch (NullPointerException e) {
-			// expected
-		}
-	}
+    @Test(expected = FileNotFoundException.class)
+    public void testShouldFailForNonExistentFile() throws IOException {
+            new FileWrapper(NON_EXISTANT_FILENAME);
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void testShouldFailForMalformedFilename() throws IOException {
+        new FileWrapper(MALFORMED_FILENAME);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testShouldFailForNullFilename() throws IOException {
+        new FileWrapper((String) null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testShouldFailForNullFilenameFile() throws IOException {
+        new FileWrapper((java.io.File) null);
+    }
 }
