2325260c91c692c3004561381e682fc6b7beeb9c
Anosh D. Ullenius <anosh@anosh.se>
2019-10-30 23:03:13 +0100

    Refactoring. Extracting magic Strings to constants

--- src/test/java/com/mpatric/mp3agic/ID3v1TagTest.java
+++ src/test/java/com/mpatric/mp3agic/ID3v1TagTest.java
@@ -6,6 +6,13 @@ import static org.junit.Assert.*;
 
 public class ID3v1TagTest {
 
+	private static final String TRACK_NUMBER = "1";
+	private static final String ALBUM = "ALBUM";
+	private static final String ARTIST = "ARTIST";
+	private static final String COMMENT = "COMMENT";
+	private static final String POP = "Pop";
+	private static final String TITLE = "TITLE";
+	private static final String YEAR = "2001";
 	private static final String VALID_TAG = "TAGTITLE1234567890123456789012345ARTIST123456789012345678901234ALBUM12345678901234567890123452001COMMENT123456789012345678901234";
 	private static final String VALID_TAG_WITH_WHITESPACE = "TAGTITLE                         ARTIST                        ALBUM                         2001COMMENT                        ";
 
@@ -29,9 +36,9 @@ public class ID3v1TagTest {
 		assertEquals("TITLE1234567890123456789012345", id3v1tag.getTitle());
 		assertEquals("ARTIST123456789012345678901234", id3v1tag.getArtist());
 		assertEquals("ALBUM1234567890123456789012345", id3v1tag.getAlbum());
-		assertEquals("2001", id3v1tag.getYear());
+		assertEquals(YEAR, id3v1tag.getYear());
 		assertEquals("COMMENT12345678901234567890123", id3v1tag.getComment());
-		assertEquals(null, id3v1tag.getTrack());
+		assertNull(id3v1tag.getTrack());
 		assertEquals(0x93, id3v1tag.getGenre());
 		assertEquals("Synthpop", id3v1tag.getGenreDescription());
 	}
@@ -46,11 +53,11 @@ public class ID3v1TagTest {
 		assertEquals("TITLE1234567890123456789012345", id3v1tag.getTitle());
 		assertEquals("ARTIST123456789012345678901234", id3v1tag.getArtist());
 		assertEquals("ALBUM1234567890123456789012345", id3v1tag.getAlbum());
-		assertEquals("2001", id3v1tag.getYear());
+		assertEquals(YEAR, id3v1tag.getYear());
 		assertEquals("COMMENT123456789012345678901", id3v1tag.getComment());
-		assertEquals("1", id3v1tag.getTrack());
+		assertEquals(TRACK_NUMBER, id3v1tag.getTrack());
 		assertEquals(0x0d, id3v1tag.getGenre());
-		assertEquals("Pop", id3v1tag.getGenreDescription());
+		assertEquals(POP, id3v1tag.getGenreDescription());
 	}
 
 	@Test
@@ -60,14 +67,14 @@ public class ID3v1TagTest {
 		buffer[buffer.length - 2] = 0x01;
 		buffer[buffer.length - 1] = 0x0D;
 		ID3v1Tag id3v1tag = new ID3v1Tag(buffer);
-		assertEquals("TITLE", id3v1tag.getTitle());
-		assertEquals("ARTIST", id3v1tag.getArtist());
-		assertEquals("ALBUM", id3v1tag.getAlbum());
-		assertEquals("2001", id3v1tag.getYear());
-		assertEquals("COMMENT", id3v1tag.getComment());
-		assertEquals("1", id3v1tag.getTrack());
+		assertEquals(TITLE, id3v1tag.getTitle());
+		assertEquals(ARTIST, id3v1tag.getArtist());
+		assertEquals(ALBUM, id3v1tag.getAlbum());
+		assertEquals(YEAR, id3v1tag.getYear());
+		assertEquals(COMMENT, id3v1tag.getComment());
+		assertEquals(TRACK_NUMBER, id3v1tag.getTrack());
 		assertEquals(0x0d, id3v1tag.getGenre());
-		assertEquals("Pop", id3v1tag.getGenreDescription());
+		assertEquals(POP, id3v1tag.getGenreDescription());
 	}
 
 	@Test
@@ -78,25 +85,25 @@ public class ID3v1TagTest {
 		buffer[buffer.length - 2] = 0x01;
 		buffer[buffer.length - 1] = 0x0D;
 		ID3v1Tag id3v1tag = new ID3v1Tag(buffer);
-		assertEquals("TITLE", id3v1tag.getTitle());
-		assertEquals("ARTIST", id3v1tag.getArtist());
-		assertEquals("ALBUM", id3v1tag.getAlbum());
-		assertEquals("2001", id3v1tag.getYear());
-		assertEquals("COMMENT", id3v1tag.getComment());
-		assertEquals("1", id3v1tag.getTrack());
+		assertEquals(TITLE, id3v1tag.getTitle());
+		assertEquals(ARTIST, id3v1tag.getArtist());
+		assertEquals(ALBUM, id3v1tag.getAlbum());
+		assertEquals(YEAR, id3v1tag.getYear());
+		assertEquals(COMMENT, id3v1tag.getComment());
+		assertEquals(TRACK_NUMBER, id3v1tag.getTrack());
 		assertEquals(0x0d, id3v1tag.getGenre());
-		assertEquals("Pop", id3v1tag.getGenreDescription());
+		assertEquals(POP, id3v1tag.getGenreDescription());
 	}
 
 	@Test
 	public void shouldGenerateValidTagBuffer() throws Exception {
 		ID3v1Tag id3v1tag = new ID3v1Tag();
-		id3v1tag.setTitle("TITLE");
-		id3v1tag.setArtist("ARTIST");
-		id3v1tag.setAlbum("ALBUM");
-		id3v1tag.setYear("2001");
-		id3v1tag.setComment("COMMENT");
-		id3v1tag.setTrack("1");
+		id3v1tag.setTitle(TITLE);
+		id3v1tag.setArtist(ARTIST);
+		id3v1tag.setAlbum(ALBUM);
+		id3v1tag.setYear(YEAR);
+		id3v1tag.setComment(COMMENT);
+		id3v1tag.setTrack(TRACK_NUMBER);
 		id3v1tag.setGenre(0x0d);
 		byte[] expectedBuffer = BufferTools.stringToByteBuffer(VALID_TAG_WITH_WHITESPACE, 0, VALID_TAG_WITH_WHITESPACE.length());
 		TestHelper.replaceSpacesWithNulls(expectedBuffer);
@@ -109,11 +116,11 @@ public class ID3v1TagTest {
 	@Test
 	public void shouldGenerateValidTagBufferWithHighGenreAndTrackNumber() throws Exception {
 		ID3v1Tag id3v1tag = new ID3v1Tag();
-		id3v1tag.setTitle("TITLE");
-		id3v1tag.setArtist("ARTIST");
-		id3v1tag.setAlbum("ALBUM");
-		id3v1tag.setYear("2001");
-		id3v1tag.setComment("COMMENT");
+		id3v1tag.setTitle(TITLE);
+		id3v1tag.setArtist(ARTIST);
+		id3v1tag.setAlbum(ALBUM);
+		id3v1tag.setYear(YEAR);
+		id3v1tag.setComment(COMMENT);
 		id3v1tag.setTrack("254");
 		id3v1tag.setGenre(0x8d);
 		byte[] expectedBuffer = BufferTools.stringToByteBuffer(VALID_TAG_WITH_WHITESPACE, 0, VALID_TAG_WITH_WHITESPACE.length());
@@ -129,13 +136,13 @@ public class ID3v1TagTest {
 		byte[] buffer = TestHelper.loadFile("src/test/resources/v1andv23tags.mp3");
 		byte[] tagBuffer = BufferTools.copyBuffer(buffer, buffer.length - ID3v1Tag.TAG_LENGTH, ID3v1Tag.TAG_LENGTH);
 		ID3v1 id3tag = new ID3v1Tag(tagBuffer);
-		assertEquals("1", id3tag.getTrack());
+		assertEquals(TRACK_NUMBER, id3tag.getTrack());
 		assertEquals("ARTIST123456789012345678901234", id3tag.getArtist());
 		assertEquals("TITLE1234567890123456789012345", id3tag.getTitle());
 		assertEquals("ALBUM1234567890123456789012345", id3tag.getAlbum());
-		assertEquals("2001", id3tag.getYear());
+		assertEquals(YEAR, id3tag.getYear());
 		assertEquals(0x0d, id3tag.getGenre());
-		assertEquals("Pop", id3tag.getGenreDescription());
+		assertEquals(POP, id3tag.getGenreDescription());
 		assertEquals("COMMENT123456789012345678901", id3tag.getComment());
 	}
 
@@ -143,12 +150,12 @@ public class ID3v1TagTest {
 	public void shouldConvertTagToBytesAndBackToEquivalentTag() throws Exception {
 		ID3v1 id3tag = new ID3v1Tag();
 		id3tag.setTrack("5");
-		id3tag.setArtist("ARTIST");
-		id3tag.setTitle("TITLE");
-		id3tag.setAlbum("ALBUM");
+		id3tag.setArtist(ARTIST);
+		id3tag.setTitle(TITLE);
+		id3tag.setAlbum(ALBUM);
 		id3tag.setYear("1997");
 		id3tag.setGenre(13);
-		id3tag.setComment("COMMENT");
+		id3tag.setComment(COMMENT);
 		byte[] data = id3tag.toBytes();
 		ID3v1 id3tagCopy = new ID3v1Tag(data);
 		assertEquals(id3tag, id3tagCopy);
